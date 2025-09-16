0a5539f1a4a7cf3ede95c531ec233f7ac19f5104
Anton <patsev.anton@gmail.com>
2025-09-16 20:08:26 +0600

    fix fmt

--- examples/1master-2nodegroup/main.tf
+++ examples/1master-2nodegroup/main.tf
@@ -60,9 +60,9 @@ module "kube" {
   node_groups = {
     "fixed-scale" = {
       subnet_ids = [module.network.private_subnets_ids[0]]
-      nat    = true
-      cores  = 2
-      memory = 4
+      nat        = true
+      cores      = 2
+      memory     = 4
       fixed_scale = {
         size = 1
       }
@@ -70,9 +70,9 @@ module "kube" {
 
     "auto-scale" = {
       subnet_ids = [module.network.private_subnets_ids[0]]
-      nat    = true
-      cores  = 2
-      memory = 8
+      nat        = true
+      cores      = 2
+      memory     = 8
       auto_scale = {
         min     = 1
         max     = 5
